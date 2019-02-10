49115f642670247d89d3baa783ef6d3fd43d04a0
paikwiki <paikwiki@gmail.com>
2019-02-11 00:29:47 +0900

    Create two dices

--- src/two-dices.php
+++ src/two-dices.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package Two_dices
- * @version 1.0.0
- */
+* @package Two_dices
+* @version 1.0.0
+*/
 /*
 Plugin Name: Two dices
 Plugin URI: #
@@ -13,7 +13,43 @@ Author URI: http://paikwiki.com
 */
 
 function two_dices() {
-  echo '<p class="dices">hello, two dices</p>';
+    $dices = [
+        '⚀',
+        '⚁',
+        '⚂',
+        '⚃',
+        '⚄',
+        '⚅',
+    ];
+    $values = [
+        rand(0, 5),
+        rand(0, 5)
+    ];
+    echo '<p class="dices">'
+    .$dices[$values[0]].$dices[$values[1]]
+    .' <span>'.(array_sum($values) + 2)
+    .'</span></p>';
 }
 
 add_action( 'admin_notices', 'two_dices' );
+
+function two_dices_css() {
+    echo "
+        <style>
+        .dices {
+            padding: 0;
+            margin: 0;
+            font-size: 1.6rem;
+            letter-spacing: 0.2rem;
+        }
+        .dices span {
+            font-size: 1rem;
+            letter-spacing: 0rem;
+        }
+        .block-editor-page .dices {
+            display: none;
+        }
+        </style>
+    ";
+}
+add_action( 'admin_head', 'two_dices_css' );
